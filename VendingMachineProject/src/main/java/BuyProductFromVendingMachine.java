fa583ca13e91e2fa62fc7e9b4be350c1bb721434
Sonal Singh Solanki <sonalsolanki2707@gmail.com>
2021-11-24 21:18:34 +0530

    Refactored code

--- VendingMachineProject/src/main/java/BuyProductFromVendingMachine.java
+++ VendingMachineProject/src/main/java/BuyProductFromVendingMachine.java
@@ -1,106 +1,150 @@
-import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class BuyProductFromVendingMachine {
 
+    public static final int MAX = 50;
+
     public static void main(String[] args) {
         //set product lists,product id and product price
-        String [] arrayOfItems = {"Choco Bar","WaterBottle","Soda Bottle"};
-        int [] arrayOfItemsId = {1,2,3};
-        float [] arrayOfItemPrice ={10.00f,23.4f,30.00f};
-        char flagToBuy ='Y';
-        int itemId=0;
-        float itemPrice=00.00f;
-        float customerBalance =00.00f;
-        float paidAmount=00.00f;
-        float vendingMachineBalance =00.00f;
+        String[] arrayOfItems = {"Choco Bar", "WaterBottle", "Soda Bottle"};
+        int[] arrayOfItemsId = {1, 2, 3};
+        float[] arrayOfItemPrice = {10.00f, 23.4f, 30.00f};
+        char flagToBuy = 'Y';
+        int itemId;
+        float itemPrice;
+        float customerBalance;
+        float paidAmount;
+        float vendingMachineBalance = 00.00f;
         //arrays to store ledger details
-        int [] arrayOfPurchasedItemsId= new int[50];
-        float [] arrayOfAmountPaidByUserForATransaction = new float[50];
-        float [] arrayOfBalanceReturnedToUserForATransaction = new float[50];
-        float [] arrayOfAmountInVendingMachine = new float[50];
-        String [] arrayOfTransactionTime = new String[50];
-        int transactionCount=0;
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-        LocalDateTime transactionTime ;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        int[] arrayOfPurchasedItemsId = new int[MAX];
+        float[] arrayOfAmountPaidByUserForATransaction = new float[MAX];
+        float[] arrayOfBalanceReturnedToUserForATransaction = new float[MAX];
+        float[] arrayOfAmountInVendingMachine = new float[MAX];
+        String[] arrayOfTransactionTime = new String[MAX];
+        int transactionCount = 0;
+
+        LocalDateTime transactionTime;
+
         String formattedDateTime;
         //System.in is a standard input stream
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         //set flag to purchase product true;
-        while (flagToBuy == 'Y'){
+        while (flagToBuy == 'Y') {
             //print the vending machine menu
-            printVendingMachineMenu(arrayOfItemsId,arrayOfItems,arrayOfItemPrice);
+            printVendingMachineMenu(arrayOfItemsId, arrayOfItems, arrayOfItemPrice);
             System.out.println("Enter the item id from menu to buy product: ");
             itemId = sc.nextInt();
-            if(itemId>5){
+            if (itemId > 5) {
                 //customer input number other than vending machine option
-                System.out.println("Invalid item id,please try again!!");
-            }else if(itemId ==5){
-                System.out.println("Printing customer ledger");
-                System.out.println("Transaction Time\t\tItemid\t\tUser Amount\t\tBalance Amount" +
-                        "\t\tVending Machine Balance");
-                for(int i=0;i<transactionCount;i++){
-                    System.out.println(arrayOfTransactionTime[i]+"\t\t"+arrayOfPurchasedItemsId[i]+"\t\t"+
-                            arrayOfAmountPaidByUserForATransaction[i]+"\t\t"+arrayOfBalanceReturnedToUserForATransaction[i]
-                            +"\t\t"+arrayOfAmountInVendingMachine[i]);
-                }
+                printInvalidMessage();
+                continue;
             }
-            else if(itemId==4){
+            if (itemId == 5) {
+                printCustomerLedger(arrayOfPurchasedItemsId, arrayOfAmountPaidByUserForATransaction, arrayOfBalanceReturnedToUserForATransaction, arrayOfAmountInVendingMachine, arrayOfTransactionTime, transactionCount);
+                continue;
+            }
+            if (itemId == 4) {
                 //customer wants to check vending machine balance
-                System.out.println("Vending Machine balance is: "+vendingMachineBalance);
-            }else
-            {//customer enter item id to buy product
-                System.out.println("Pay the amount: ");
-                paidAmount = sc.nextInt();
-
-                itemPrice = arrayOfItemPrice[itemId - 1];
-                //check if customer paid amount less than item price
-                if (itemPrice <= paidAmount)
-                {
-                    transactionTime = LocalDateTime.now(); //save the transaction time
-                    formattedDateTime = transactionTime.format(formatter);
-                    vendingMachineBalance = vendingMachineBalance + itemPrice;
-                    customerBalance = buyProduct(itemPrice, paidAmount, customerBalance);
-                    arrayOfAmountInVendingMachine[transactionCount] = vendingMachineBalance;
-                    arrayOfBalanceReturnedToUserForATransaction[transactionCount] = customerBalance;
-                    arrayOfAmountPaidByUserForATransaction[transactionCount] = paidAmount;
-                    arrayOfPurchasedItemsId[transactionCount] = itemId;
-                    arrayOfTransactionTime[transactionCount] = formattedDateTime;
-                    transactionCount++;
-
-                    System.out.println("Returning balance amount to customer : " + customerBalance + ".\n" +
-                            "Dispensing the product.Thank you for purchasing the product");
-                } else{
-                    //customer paid less amount
-                    System.out.println("Insufficient amount,please try again ");
-                }
+                printVendingMachineBalance(vendingMachineBalance);
+                continue;
             }
+            //customer enter item id to buy product
+            System.out.println("Pay the amount: ");
+            paidAmount = sc.nextInt();
+
+            itemPrice = arrayOfItemPrice[itemId - 1];
+            //check if customer paid amount less than item price
+            if (itemPrice <= paidAmount) {
+                vendingMachineBalance = vendingMachineBalance + itemPrice;
+                customerBalance = buyProduct(itemPrice, paidAmount);
+                updateTransaction(itemId, customerBalance, paidAmount, vendingMachineBalance, arrayOfPurchasedItemsId, arrayOfAmountPaidByUserForATransaction, arrayOfBalanceReturnedToUserForATransaction, arrayOfAmountInVendingMachine, transactionCount);
+                updateTransactionTime(arrayOfTransactionTime, transactionCount);
+
+                transactionCount++;
+
+                printBalanceMessage(customerBalance);
+            } else {
+                //customer paid less amount
+                System.out.println("Insufficient amount,please try again ");
+            }
+
             System.out.println("Do you want to buy another item,press Y/y-Yes and N/n -No: ");
             flagToBuy = sc.next().charAt(0);
             //converts user input to upper case
-            flagToBuy=Character.toUpperCase(flagToBuy);
+            flagToBuy = Character.toUpperCase(flagToBuy);
         }
 
         System.out.println("Thank you!!");
     }
 
-    static void printVendingMachineMenu(int[] arrayOfItemsId, String[] arrayOfItems, float[] arrayOfItemPrice){
-        {
-            System.out.println("Select itemid from the menu to buy the product :");
-            System.out.println("ItemId \t\tItem Name\t\tItem Price");
-            for(int i=0;i<arrayOfItemsId.length;i++){
-                System.out.println(arrayOfItemsId[i]+"\t\t\t"+arrayOfItems[i]+"\t\t\t"+arrayOfItemPrice[i] );
-            }
-            System.out.println("4 \t\t\tVending Machine Balance " );
-            System.out.println("5 \t\t\tUser Transaction History " );}
+    private static void printBalanceMessage(float customerBalance) {
+        System.out.println("Returning balance amount to customer : " + customerBalance + ".\n" +
+                "Dispensing the product.Thank you for purchasing the product");
     }
-    static float buyProduct(float itemPrice, float paidAmount, float customerBalance) {
+
+    private static void updateTransactionTime(String[] arrayOfTransactionTime, int transactionCount) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
+        arrayOfTransactionTime[transactionCount] = getFormattedDate(formatter);
+    }
+
+    private static void updateTransaction(int itemId, float customerBalance, float paidAmount, float vendingMachineBalance, int[] arrayOfPurchasedItemsId, float[] arrayOfAmountPaidByUserForATransaction, float[] arrayOfBalanceReturnedToUserForATransaction, float[] arrayOfAmountInVendingMachine, int transactionCount) {
+        arrayOfAmountInVendingMachine[transactionCount] = vendingMachineBalance;
+        arrayOfBalanceReturnedToUserForATransaction[transactionCount] = customerBalance;
+        arrayOfAmountPaidByUserForATransaction[transactionCount] = paidAmount;
+        arrayOfPurchasedItemsId[transactionCount] = itemId;
+    }
+
+    private static String getFormattedDate(DateTimeFormatter formatter) {
+        LocalDateTime transactionTime;
+        String formattedDateTime;
+        transactionTime = LocalDateTime.now(); //save the transaction time
+        formattedDateTime = transactionTime.format(formatter);
+        return formattedDateTime;
+    }
+
+    private static void printVendingMachineBalance(float vendingMachineBalance) {
+        System.out.println("Vending Machine balance is: " + vendingMachineBalance);
+    }
+
+    private static void printCustomerLedger(int[] arrayOfPurchasedItemsId,
+                                            float[] arrayOfAmountPaidByUserForATransaction, float[] arrayOfBalanceReturnedToUserForATransaction, float[] arrayOfAmountInVendingMachine, String[] arrayOfTransactionTime, int transactionCount) {
+       if(transactionCount ==0){
+           System.out.println("No ldeger records found!!");
+       }else{
+        System.out.println("Printing customer ledger");
+        System.out.println("Transaction Time\t\tItemId\t\tUser Amount\t\tBalance Amount" +
+                "\t\tVending Machine Balance");
+           System.out.println("=================================================================");
+        for (int i = 0; i < transactionCount; i++) {
+            System.out.println(arrayOfTransactionTime[i] + "\t\t" + arrayOfPurchasedItemsId[i] + "\t\t" +
+                    arrayOfAmountPaidByUserForATransaction[i] + "\t\t" + arrayOfBalanceReturnedToUserForATransaction[i]
+                    + "\t\t" + arrayOfAmountInVendingMachine[i]);
+        }
+       }
+    }
+
+    private static void printInvalidMessage() {
+        System.out.println("Invalid item id,please try again!!");
+    }
+
+    static void printVendingMachineMenu(int[] arrayOfItemsId, String[] arrayOfItems, float[] arrayOfItemPrice) {
+        {
+            System.out.println("Select itemId from the menu to buy the product :");
+            System.out.println("ItemId \t\tItem Name\t\tItem Price");
+            System.out.println("=================================================================");
+            for (int i = 0; i < arrayOfItemsId.length; i++) {
+                System.out.println(arrayOfItemsId[i] + "\t\t\t" + arrayOfItems[i] + "\t\t\t" + arrayOfItemPrice[i]);
+            }
+            System.out.println("4 \t\t\tVending Machine Balance ");
+            System.out.println("5 \t\t\tUser Transaction History ");
+        }
+    }
+
+    static float buyProduct(float itemPrice, float paidAmount) {
         //method to calculate customer transaction for an item
-        customerBalance = paidAmount - itemPrice;
-        return customerBalance;
+        return paidAmount - itemPrice;
     }
 
 }
